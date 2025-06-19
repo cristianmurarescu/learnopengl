56b00b999910c31651d019cb89a6db8e798ab3a4
Cristian Murarescu <cristian.murarescu@unity3d.com>
2025-06-19 13:42:40 +0100

    create a vertex attribute & render triangle using default shader

--- LearnOpenGL/Source.cpp
+++ LearnOpenGL/Source.cpp
@@ -7,11 +7,9 @@ int main(void)
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
+    /* Initialize the GLFW library */
     if (!glfwInit())
         return -1;
-
-
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -24,13 +22,16 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* Initialize the GLEW library */
     if (glewInit() != GLEW_OK)
     {
         std::cout << "Error!" << std::endl;
     }
 
+    /* Print out the GL version */
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    /* Vertex data for our triangle */
     float positions[6] = 
     {
         -0.5f, -0.5f,
@@ -38,7 +39,7 @@ int main(void)
          0.5f, -0.5f,
     };
 
-    /* Create a buffer and pass it our data */
+    /* Create a buffer */
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     
@@ -48,6 +49,12 @@ int main(void)
     /* Fill in buffer with our data */
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
+    /* Enable the vertex attribute pointer */
+    glEnableVertexAttribArray(0);
+
+    /* Set the vertex attribute pointer data */
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -55,6 +62,7 @@ int main(void)
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        /* Draw the arrays of data using the triangles primitive */
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
